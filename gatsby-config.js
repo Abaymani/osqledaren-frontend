b1766e4377b715c63b483970c7e2dba5a6aabd0e
Markus Wagner <prttmprpht@gmail.com>
2023-04-20 11:32:03 +0200

    Try to add netlify support for gatsby redirects

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,47 @@
 /* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   siteMetadata: {
-    title: 'Osqledaren',
+    title: "Osqledaren",
   },
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/src/images`,
-        name: 'images',
+        name: "images",
       },
     },
     {
-      resolve: 'gatsby-source-podcast',
+      resolve: "gatsby-source-podcast",
       options: {
-        podcasts: ['https://anchor.fm/s/17740998/podcast/rss'],
+        podcasts: ["https://anchor.fm/s/17740998/podcast/rss"],
       },
     },
     {
-      resolve: 'gatsby-source-sanity',
+      resolve: "gatsby-source-sanity",
       options: {
-        projectId: 'ih69fm79',
-        dataset: 'production',
+        projectId: "ih69fm79",
+        dataset: "production",
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-emotion',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-react-helmet',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: 'Osqledaren',
-        short_name: 'OL',
-        start_url: '/',
-        background_color: '#fff',
-        theme_color: '#fff',
-        display: 'minimal-ui',
-        icon: './src/images/logo-small.png', // This path is relative to the root of the site.
+        name: "Osqledaren",
+        short_name: "OL",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#fff",
+        display: "minimal-ui",
+        icon: "./src/images/logo-small.png", // This path is relative to the root of the site.
       },
     },
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
+    "gatsby-plugin-netlify-redirect",
   ],
 };
