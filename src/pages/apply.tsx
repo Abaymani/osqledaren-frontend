e540d69fd44ee4509b3dfd1df91f8e0a4e362515
Benjamin <benjamin@osqledaren.se>
2023-08-02 15:06:56 +0200

    Design new application page that redirects to Google forms

--- src/pages/apply.tsx
+++ src/pages/apply.tsx
@@ -1,33 +1,157 @@
 import React, { FunctionComponent } from "react";
 import styled from "../styles/styled";
-import Layout from "../organisms/Layout";
+import logo from "../images/logo-black.png";
+import OLstructure from "../images/OLStructure.svg";
+import Helmet from "react-helmet";
 import P from "../atoms/P";
+import ExternalLink from "../atoms/Link";
 
 const Apply: FunctionComponent = () => {
-  if (typeof window !== `undefined`) {
-    window.location.href = "https://forms.gle/hnN5VNgEK8hZHRzUA";
-  }
-
   return (
     <Layout>
-      <RedirectMessageWrapper>
-        <P>Redirecting...</P>
-      </RedirectMessageWrapper>
+      <Helmet>
+        <title>Apply - Osqledaren</title>
+      </Helmet>
+
+      <Header>
+        <Logo src={logo}></Logo>
+      </Header>
+
+      <LinkListContainer>
+        <OLStructure src={OLstructure}></OLStructure>
+        <LinkList>
+          <Link
+            href="https://forms.gle/kr1G7sN2JPRAZsqMA"
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              backgroundColor: "#cccccc",
+              color: "black"
+            }}
+          >
+            {"Apply for Osqledingen"}
+
+            <LinkImage></LinkImage>
+          </Link>
+
+          <Link
+            href="https://forms.gle/GD3wncsaTAcwsN5u8"
+            target="_blank"
+            rel="noreferrer"
+            style={{
+              backgroundColor: "#cccccc",
+              color: "black"
+            }}
+          >
+            {"Apply for the Redaqtion"}
+            <LinkImage></LinkImage>
+          </Link>
+        </LinkList>
+      </LinkListContainer>
+
+      <Footer>
+        <P size="5pt" color="white">
+          © 2008 - {new Date().getFullYear()} Osqledaren.
+        </P>
+      </Footer>
     </Layout>
   );
 };
 
 export default Apply;
 
-const RedirectMessageWrapper = styled.div`
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+  max-height: 100vh;
+  font-family: Avenir, sans-serif;
+  overscroll-behavior: none;
+  font-size: 2.5vh;
+`;
+
+const Header = styled.div`
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  flex-shrink: 0;
+  height: 12vh;
+  box-shadow: -6px 0 white, 6px 0 white, 0 0px 20px grey;
+`;
+
+const LinkListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 100%;
-  padding: 0 5%;
-  margin: 20px 0;
-  justify-content: space-between;
-  @media (max-width: ${({ theme }) => theme.breakpoints.xl + 350 + "px"}) {
-    flex-direction: column;
+  flex-grow: 1;
+  overflow-y: auto;
+`;
+
+const OLStructure = styled.img`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 80%;
+`;
+
+const LinkList = styled.div`
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 80%;
+`;
+
+const LinkImage = styled.img`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  height: 40px;
+`;
+
+const Link = styled.a`
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background: rgb(220, 112, 45);
+  color: inherit;
+  text-decoration: inherit;
+  height: 70px;
+  font-weight: bold;
+  border-radius: 10px;
+
+  &:not(:last-child) {
+    margin-bottom: 10px;
   }
+  &:hover {
+    transform: scale(1.02);
+    transition: transform 0.3s;
+  }
+`;
+
+const Logo = styled.img`
+  height: 10vh;
+`;
+
+const Footer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  flex-shrink: 0;
+  height: 50px;
+  background-color: black;
+  box-sizing: border-box;
+  font-size: 2vh;
 `;
